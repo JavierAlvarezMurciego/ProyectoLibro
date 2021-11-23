e58bfe15926c7415ecf6dbeae306eea8eb54da19
Javier <alvmurjav@gmail.com>
2021-11-23 10:51:16 +0100

    1847.05 Ahora devuelve el número de referencia y da un error si este es menor de tres caracteres

--- Libro.java
+++ Libro.java
@@ -13,6 +13,7 @@ public class Libro {
     private String autor;
     private String titulo;
     private int numeroPaginas;
+    private String numeroReferencia;
 
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
@@ -22,34 +23,61 @@ public class Libro {
         autor = autorLibro;
         titulo = tituloLibro;
         numeroPaginas = numeroDePaginas;
+        numeroReferencia = "";
     }
-    
+
     public String getAutor(){
         return autor;
     }
-    
+
     public String getTitulo(){
         return titulo;
     }
-    
+
     public void imprimeAutor(){
-       System.out.println(autor); 
+        System.out.println(autor); 
     }
-    
+
     public void imprimeTitulo(){
         System.out.println(titulo); 
     }
-    
+
     public int getNumeroPaginas(){
         return numeroPaginas;
     }
-    
+
     public void imprimirDetalles(){
-        System.out.println("Autor: "+ autor + ", "+"Título: "+ titulo+", "+"Páginas: "+ numeroPaginas);
+        if(numeroReferencia.length()==0){
+            System.out.println("Autor: "+ autor + ", "+"Título: "+ titulo+", "+"Páginas: "+ numeroPaginas);
+            System.out.println("Número de referencia: ZZZ"); 
+        }
+        else {
+            System.out.println("Autor: "+ autor + ", "+"Título: "+ titulo+", "+"Páginas: "+ numeroPaginas);
+            System.out.println("Número de referencia: "+ numeroReferencia);
+        }
     }
-    
+
     public String getDetalles(){
-        String detalles = "Autor del libro: " +autor+", "+"Título del libro: "+titulo+", "+"Número de páginas: "+numeroPaginas;
-        return detalles;
+        String aDevolver = "";
+        if(numeroReferencia.length() == 0){ 
+            aDevolver = "Autor del libro: " +autor+", "+"Título del libro: "+titulo+", "+"Número de páginas: "+numeroPaginas;
+            System.out.println("Número de referencia: ZZZ"); 
+        }
+        else{
+            aDevolver = "Autor del libro: " +autor+", "+"Título del libro: "+titulo+", "+"Número de páginas: "+numeroPaginas;
+            System.out.println("Número de referencia: "+ numeroReferencia);
+        }
+        return aDevolver;
+    }
+
+    public String getNumeroReferencia(){
+        return numeroReferencia;
+    }
+
+    public void setNumeroReferencia(String nuevoNumeroReferencia){
+        numeroReferencia = nuevoNumeroReferencia;
+        if(numeroReferencia.length() <= 3){
+            System.out.println("Error: Solo acepta un número de referencia que contenga al menos tres caracteres");
+        }
     }
 }
